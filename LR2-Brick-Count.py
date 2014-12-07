885953b5b711308900ff5321b6c5e941b9de6af6
Triangle717 <le717.code@yahoo.com>
2014-12-06 19:44:05 -0500

    Update LR2-Brick-Count.py

--- LR2-Brick-Count.py
+++ LR2-Brick-Count.py
@@ -1,48 +1,68 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
+from __future__ import print_function
 import os
+import sys
 
-inFile = "UNIQUEBRICKS.TXT"
-doNotExist = []
 
-with open(inFile, "rt") as f:
-    lines = f.readlines()
+def main():
+    """The entire application."""
+    # We were not given enough arguments
+    if len(sys.argv) != 2:
+        print("Usage: {0} [Input file]".format(
+              os.path.basename(sys.argv[0])))
+        raise SystemExit(1)
 
-brickCount = 0
-for line in lines:
+    # Setup required items
+    inFile = os.path.abspath(sys.argv[1])
+    brickCount = 0
+    doNotExist = []
 
-    # Do not process comments or blank lines
-    if not line.startswith("#") and line not in ("\r", "\n", "\r\n"):
-        brickCount += 1
+    # The input file does not exist
+    if not os.path.exists(inFile):
+        print("{0} does not exist!".format(inFile))
+        raise SystemExit(1)
 
-        # Extract only the file path to the model, removing unneeded path
-        line = line[:line.find("MD2") + 3].split("\\")[3:]
+    # Read the file
+    with open(inFile, "rt") as f:
+        lines = f.readlines()
 
-        # Construct the path to the 3D model
-        modelPath = os.path.join(os.getcwd(), os.path.sep.join(line))
+    for line in lines:
+        # Do not process comments or blank lines
+        if not line.startswith("#") and line not in ("\r", "\n", "\r\n"):
+            brickCount += 1
 
-        # The 3D model does exist, move along
-        if os.path.isfile(modelPath):
-            continue
+            # Extract only the file path to the model, removing unneeded path
+            line = line[:line.find("MD2") + 3].split("\\")[3:]
+
+            # Construct the path to the 3D model
+            modelPath = os.path.join(os.getcwd(), os.path.sep.join(line))
+
+            # The 3D model does exist, move along
+            if os.path.isfile(modelPath):
+                continue
 
             # Nope, it does not exist
-        else:
-            doNotExist.append(os.path.sep.join(line))
+            else:
+                doNotExist.append(os.path.sep.join(line))
 
-# Report the number of bricks
-print("There are {0} bricks listed in {1}.\n".format(brickCount,
-      os.path.basename(inFile)))
+    # Report the number of bricks
+    print("There are {0} bricks listed in {1}.\n".format(brickCount,
+          os.path.basename(inFile)))
 
-# Some 3D models do not exist, report them
-if len(doNotExist) > 0:
-    print("Of the bricks listed, {0} do not exist. They are as follows:"
-          .format(len(doNotExist)))
+    # Some 3D models do not exist, report them
+    if len(doNotExist) > 0:
+        print("Of the bricks listed, {0} do not exist. They are as follows:\n"
+              .format(len(doNotExist)))
 
-    for part in doNotExist:
-        print("* {0}".format(part))
+        for part in doNotExist:
+            print("* {0}".format(part))
 
-# All models exist
-else:
-    print("All {0} bricks exist.".format(brickCount))
+    # All models exist
+    else:
+        print("All {0} bricks exist.".format(brickCount))
+    raise SystemExit(0)
+
+if __name__ == "__main__":
+    main()
